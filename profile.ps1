113efc41afe90086c0bb8231e81e7bcf6f834095
xiote <xiote@nate.com>
2020-05-18 23:23:20 +0900

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -20,7 +20,7 @@ function src {
 }
 
 function vimcnf {
-	rd /s ~\vimfiles\autoload
+	rd ~\vimfiles\autoload /s
 	md ~\vimfiles\autoload
 	$uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 	(New-Object Net.WebClient).DownloadFile(
