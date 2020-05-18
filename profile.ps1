aa4fde9f1aa0bf9525ca2c20192beab33b527e8d
xiote <xiote@nate.com>
2020-05-18 22:23:59 +0900

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -18,3 +18,20 @@ function src {
 	curl https://raw.githubusercontent.com/xiote/pwsh/master/profile.ps1 -o .\profile.ps1
 	. .\profile.ps1
 }
+
+function vimcnf {
+    rm -r ~/.vim
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        
+    rm ~/.vimrc
+    curl -L https://raw.githubusercontent.com/xiote/vim/master/.vimrc -o ~/.vimrc
+
+#    mkdir -p ~/.vim/pack/plugins/start
+
+#    git clone https://github.com/vimwiki/vimwiki.git ~/.vim/pack/plugins/start/vimwiki;
+#    git clone https://tpope.io/vim/fugitive.git ~/.vim/pack/plugins/start/fugitive;
+#    git clone https://github.com/xiote/link.vim.git ~/.vim/pack/plugins/start/link.vim;
+#    git clone https://github.com/vim-syntastic/syntastic.git ~/.vim/pack/plugins/start/syntastic
+#    git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
+}
