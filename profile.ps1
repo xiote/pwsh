4445f3f114b2726dc07dae5b31127378466def68
xiote <xiote@nate.com>
2020-05-18 20:08:02 +0900

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,11 @@
+# 프로파일 생성
+# http://gitlab.krei.co.kr:90/nextep/common/wikis/powershell/프로파일-생성
+
+# include lines in profile.ps1
+
+#curl https://raw.githubusercontent.com/xiote/zsh/master/profile.ps1 -OutFile .\profile.ps1
+#. .\profile.ps1
+
 set-alias cat2 get-content
 
 function aliascheck {
