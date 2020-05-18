e8da202cef7a1c5b80ee32c61285e853fefed5d2
xiote <xiote@nate.com>
2020-05-18 23:17:29 +0900

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -20,6 +20,7 @@ function src {
 }
 
 function vimcnf {
+	rd ~\vimfiles\autoload
 	md ~\vimfiles\autoload
 	$uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 	(New-Object Net.WebClient).DownloadFile(
@@ -29,6 +30,12 @@ function vimcnf {
 	  )
 	)
 	
-	rm "~\.vimrc"
-	curl -L https://raw.githubusercontent.com/xiote/vim/master/.vimrc -o "~\.vimrc"
+	rm ~\.vimrc
+	$uri = 'https://raw.githubusercontent.com/xiote/vim/master/.vimrc'
+	(New-Object Net.WebClient).DownloadFile(
+	  $uri,
+	  $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
+	    "~\.vimrc"
+	  )
+	)
 }
