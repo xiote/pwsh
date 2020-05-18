76558943648196608e9dd9a58f1846f87d42091b
xiote <xiote@nate.com>
2020-05-18 22:55:31 +0900

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -3,9 +3,9 @@
 
 # include lines in profile.ps1
 
-#rm .\profile.ps1
-#curl https://raw.githubusercontent.com/xiote/pwsh/master/profile.ps1 -o .\profile.ps1
-#. .\profile.ps1
+#rm $home\profile.ps1
+#curl https://raw.githubusercontent.com/xiote/pwsh/master/profile.ps1 -o $home\profile.ps1
+#. $home\profile.ps1
 
 set-alias cat2 get-content
 
@@ -14,14 +14,14 @@ function aliascheck {
 }
 
 function src {
-	rm .\profile.ps1
-	curl https://raw.githubusercontent.com/xiote/pwsh/master/profile.ps1 -o .\profile.ps1
-	. .\profile.ps1
+	rm $home\profile.ps1
+	curl https://raw.githubusercontent.com/xiote/pwsh/master/profile.ps1 -o $home\profile.ps1
+	. $home\profile.ps1
 }
 
 function vimcnf {
-    rm -r ~\.vim
-    curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -fLo ~\.vim\autoload\plug.vim --create-dirs
-    rm ~\.vimrc
-    curl -L https://raw.githubusercontent.com/xiote/vim/master/.vimrc -o ~\.vimrc
+    rm -r $home\.vim
+    curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -fLo $home\.vim\autoload\plug.vim --create-dirs
+    rm $home\.vimrc
+    curl -L https://raw.githubusercontent.com/xiote/vim/master/.vimrc -o $home\.vimrc
 }
