343b1669b5305314d8b91f6a25d4bcf506f5633a
xiote <xiote@nate.com>
2020-05-18 21:21:10 +0900

    오타 수정

--- profile.ps1
+++ profile.ps1
@@ -3,7 +3,7 @@
 
 # include lines in profile.ps1
 
-#curl https://raw.githubusercontent.com/xiote/zsh/master/profile.ps1 -OutFile .\profile.ps1
+#curl https://raw.githubusercontent.com/xiote/pwsh/master/profile.ps1 -OutFile .\profile.ps1
 #. .\profile.ps1
 
 set-alias cat2 get-content
